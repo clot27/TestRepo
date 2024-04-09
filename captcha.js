a848a9ecb8e7444ec2de508dd83ec7ad88dcfe25
Aditya Yadav <69784758+clot27@users.noreply.github.com>
2024-04-09 12:04:16 +0000

    Update captcha.js

--- captcha.js
+++ captcha.js
@@ -3,13 +3,7 @@ function rotateImage(degrees) {
     var currentRotation = getComputedStyle(captchaImage).transform;
     var values = currentRotation.match(/matrix.*\((.+)\)/)[1].split(', ');
     var angle = Math.round(Math.atan2(values[1], values[0]) * (180/Math.PI));
-
-    // Calculate the new rotation angle
     var newAngle = angle + degrees;
-
-    // Apply the new rotation
     captchaImage.style.transform = `rotate(${newAngle}deg)`;
 }
-
-// Initially, ensure the image is not rotated
 document.getElementById('captcha-image').style.transform = 'rotate(0deg)';
